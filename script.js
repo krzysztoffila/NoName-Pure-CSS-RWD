818c2acd4a1723c6a24fb014a5b944bcd228fe69
krzysztoffila <umowy.fila@gmail.com>
2024-03-30 14:17:27 +0100

    [FIXED][v2] - Navigation styles

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerMenu = document.querySelector(".hamburger-menu");
-  const menuLinks = document.querySelector(".menu-links");
-  console.log('clicked');
-  hamburgerMenu.addEventListener("click", function () {
-    menuLinks.classList.toggle("active");
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerIcon = document.querySelector('.hamburger-icon');
+  const mobileMenu = document.querySelector('.navigation__mobile--menu');
+
+  hamburgerIcon.addEventListener('click', function () {
+    mobileMenu.classList.toggle('open'); // Dodaj lub usuń klasę open, aby pokazać lub ukryć menu hamburgerowe
   });
 });
