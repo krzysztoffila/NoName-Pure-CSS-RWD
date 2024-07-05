7217c03b166a251d76013a0255b47434e5e07b38
krzysztoffila <umowy.fila@gmail.com>
2024-07-05 16:31:31 +0200

    Add smooth scrolling

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const hamburgerIcon = document.querySelector('.hamburger-icon');
-  const mobileMenu = document.querySelector('.navigation__mobile--menu');
-
-  hamburgerIcon.addEventListener('click', function () {
-    mobileMenu.classList.toggle('open');
-  });
+"use strict";
+// Smooth scrolling
+const introSection = document.querySelector(".intro");
+const introLink = document.querySelector(".intro--link");
+const keyPointsSection = document.querySelector(".key__points");
+const keyPointsLink = document.querySelector(".key__points--link");
+const statsSection = document.querySelector(".stats");
+const statsLink = document.querySelector(".stats--link");
+const featuresSection = document.querySelector(".features");
+const featuresLink = document.querySelector(".features--link");
+const authorsSection = document.querySelector(".authors");
+const authorLink = document.querySelector(".authors--link");
+const pricingSection = document.querySelector(".pricing");
+const pricingLink = document.querySelector(".pricing--link");
+introLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  introSection.scrollIntoView({ behavior: "smooth" });
+});
+keyPointsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  keyPointsSection.scrollIntoView({ behavior: "smooth" });
+});
+statsLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  statsSection.scrollIntoView({ behavior: "smooth" });
+});
+featuresLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  featuresSection.scrollIntoView({ behavior: "smooth" });
+});
+authorLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  authorsSection.scrollIntoView({ behavior: "smooth" });
+});
+pricingLink.addEventListener("click", (e) => {
+  e.preventDefault();
+  pricingSection.scrollIntoView({ behavior: "smooth" });
 });
